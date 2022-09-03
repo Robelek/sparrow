d062b113d22683bf3ebdb09d35a7265e6c44a6be
Robeleczek <robeleczek@gmail.com>
2022-09-03 18:37:23 +0200

    Finally fixed...?

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.resolve(__dirname + "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/api", (req, res)=>
 {
     res.json("Hello there!");
@@ -9,7 +9,7 @@ app.get("/api", (req, res)=>
 
 app.get("*", (req, res)=>
 {
-    res.sendFile(express.static(path.resolve(__dirname, "../client/build","index.html")))
+    res.sendFile(express.static(path.join(__dirname, "../client/public","index.html")));
 })
 
 app.listen(3001 || process.env.PORT);
