89f97e51cef2369c113bad0f787d20177245076e
Robeleczek <robeleczek@gmail.com>
2022-09-03 15:12:25 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.resolve(__dirname + "../client/build")));
+app.get("/api", (req, res)=>
+{
+    res.json("Hello there!");
+})
+
+app.get("*", (req, res)=>
+{
+    res.sendFile(express.static(path.resolve(__dirname, "../client/build","index.html")))
+})
+
+app.listen(3001 || process.env.PORT);
